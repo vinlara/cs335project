f2201b0f815684a3f02369afe2f8c5fbd52b0cc7
student <student@lab-315-012.cs.csubak.edu>
2015-10-23 15:16:10 -0700

    kinda fixed (not really)

--- ericS.cpp
+++ ericS.cpp
@@ -36,7 +36,8 @@ struct Ship {
         pos[1] = (Flt)(960/2);
         pos[2] = 0.0f;
         VecZero(vel);
-        angle = 0.0;
+        radius = 40.0;
+	angle = 0.0;
         color[0] = 1.0;
         color[1] = 1.0;
         color[2] = 1.0;
