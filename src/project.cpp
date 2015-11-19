18b06e3ae9c4f64f66d3c517382f3bc9892a58e5
esmtih8900 <esmith8900@gmail.com>
2015-11-18 22:51:24 -0800

    scaled score

--- src/project.cpp
+++ src/project.cpp
@@ -377,7 +377,7 @@ void physics()
 				if (a->radius > 0)
                 		{
                     			int radiusDif = g.ship.radius - a->radius;
-					g.score += 1 / radiusDif;
+					g.score += 10.0 / radiusDif;
 		    			//g.score += 0.5 * a->radius;
                 		}
 
