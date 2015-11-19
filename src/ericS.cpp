1fdcff405b20bc6d47d6e306aecd7e56030d6fe3
Perry Huynh <huynh.perry@gmail.com>
2015-11-18 23:27:30 -0800

    fixed indenting

--- src/ericS.cpp
+++ src/ericS.cpp
@@ -1,15 +1,15 @@
 /*  Eric Smith
     ericS.cpp
 
-	CS 335 Software Engineering Project
-	Super Dank Space Adventure
+    CS 335 Software Engineering Project
+    Super Dank Space Adventure
 
-	Group Members:
-	Eric Smith
-	Erik Juarez
-	Perry Huynh
-	Vincente Lara
-*/
+    Group Members:
+    Eric Smith
+    Erik Juarez
+    Perry Huynh
+    Vincente Lara
+ */
 
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
@@ -31,84 +31,84 @@ void init()
     //build 30 asteroids...
     for ( int j=0; j<30; j++ )
     {
-	Asteroid *a = new Asteroid;
-	a->nverts = 8;
-	a->radius = ( rnd() * 2.0 * g.ship.radius ) - ( rnd() * 0.8 * g.ship.radius  );
-	a->textureId = rand() % 6;
-	Flt r2 = a->radius / 2.0;
-	Flt angle = 0.0f;
-	Flt inc = (PI * 2.0) / (Flt)a->nverts;
-	
-	for (int i=0; i<a->nverts; i++)
-	{
-	    a->vert[i][0] = sin( angle ) * ( r2 + rnd() * a->radius );
-	    a->vert[i][1] = cos( angle ) * ( r2 + rnd() * a->radius );
-	    angle += inc;
-	}
-	
-	a->pos[0] = ( Flt ) ( rand() % xres );
-	a->pos[1] = ( Flt ) ( rand() % yres );
-	a->pos[2] = 0.0f;
-	a->angle = 0.0;
-	a->rotate = rnd() * 4.0 - 2.0;
-	
-	//protect area near center from bigger asteroids
-	Flt tmpx = xres >> 2;
-	Flt tmpy = yres >> 2;
-	
-	if ( a->pos[0] <= ( 3.0 * tmpx ) &&
-		a->pos[0] >= tmpx &&
-		a->pos[1] <= ( 3.0 * tmpy ) &&
-		a->pos[1] >= tmpy
-	   )
-	{
-	    
-	    if (a->pos[0] <= ( 2.0 * tmpx ) &&
-		    a->pos[0] > tmpx
-	       ) //left half
-	    {
-		a->pos[0] -= 1.25 * tmpx;
-		a->pos[2] = 0.0f;
-	    }
-	    
-	    else //right half
-	    {
-		a->pos[0] += 1.25 * tmpx;
-		a->pos[2] = 0.0f;
-	    }
-	}
-	
-	if ( a->radius < g.ship.radius )
-	{
-	    a->color[0] = 0.9;
-	    a->color[1] = 0.6;
-	    a->color[2] = 0.3;
-	}
-	
-	else
-	{
-	    a->color[0] = 0.3;
-	    a->color[1] = 0.4;
-	    a->color[2] = 0.5;
-	}
-	
-	a->vel[0] = ( Flt ) ( rnd() * 2.0 - 1.0 );
-	a->vel[1] = ( Flt ) ( rnd() * 2.0 - 1.0 );
-	
-	asteroidRadiusSpeed(a);
-	
-	//add to front of linked list
-	a->next = g.ahead;
-	
-	if ( g.ahead != NULL )
-	{
-	    g.ahead->prev = a;
-	}
+        Asteroid *a = new Asteroid;
+        a->nverts = 8;
+        a->radius = ( rnd() * 2.0 * g.ship.radius ) - ( rnd() * 0.8 * g.ship.radius  );
+        a->textureId = rand() % 6;
+        Flt r2 = a->radius / 2.0;
+        Flt angle = 0.0f;
+        Flt inc = (PI * 2.0) / (Flt)a->nverts;
 
-	g.ahead = a;
-	g.nasteroids++;
+        for (int i=0; i<a->nverts; i++)
+        {
+            a->vert[i][0] = sin( angle ) * ( r2 + rnd() * a->radius );
+            a->vert[i][1] = cos( angle ) * ( r2 + rnd() * a->radius );
+            angle += inc;
+        }
+
+        a->pos[0] = ( Flt ) ( rand() % xres );
+        a->pos[1] = ( Flt ) ( rand() % yres );
+        a->pos[2] = 0.0f;
+        a->angle = 0.0;
+        a->rotate = rnd() * 4.0 - 2.0;
+
+        //protect area near center from bigger asteroids
+        Flt tmpx = xres >> 2;
+        Flt tmpy = yres >> 2;
+
+        if ( a->pos[0] <= ( 3.0 * tmpx ) &&
+                a->pos[0] >= tmpx &&
+                a->pos[1] <= ( 3.0 * tmpy ) &&
+                a->pos[1] >= tmpy
+           )
+        {
+
+            if (a->pos[0] <= ( 2.0 * tmpx ) &&
+                    a->pos[0] > tmpx
+               ) //left half
+            {
+                a->pos[0] -= 1.25 * tmpx;
+                a->pos[2] = 0.0f;
+            }
+
+            else //right half
+            {
+                a->pos[0] += 1.25 * tmpx;
+                a->pos[2] = 0.0f;
+            }
+        }
+
+        if ( a->radius < g.ship.radius )
+        {
+            a->color[0] = 0.9;
+            a->color[1] = 0.6;
+            a->color[2] = 0.3;
+        }
+
+        else
+        {
+            a->color[0] = 0.3;
+            a->color[1] = 0.4;
+            a->color[2] = 0.5;
+        }
+
+        a->vel[0] = ( Flt ) ( rnd() * 2.0 - 1.0 );
+        a->vel[1] = ( Flt ) ( rnd() * 2.0 - 1.0 );
+
+        asteroidRadiusSpeed(a);
+
+        //add to front of linked list
+        a->next = g.ahead;
+
+        if ( g.ahead != NULL )
+        {
+            g.ahead->prev = a;
+        }
+
+        g.ahead = a;
+        g.nasteroids++;
     }
-    
+
     memset(keys, 0, 65536);
 }
 
@@ -125,14 +125,14 @@ void addAsteroid ()
     Flt r2 = a->radius / 2.0;
     Flt angle = 0.0f;
     Flt inc = ( PI * 2.0 ) / (Flt)a->nverts;
-    
+
     for ( int i=0; i<a->nverts; i++ )
     {
         a->vert[i][0] = sin( angle ) * ( r2 + rnd() * a->radius );
         a->vert[i][1] = cos( angle ) * ( r2 + rnd() * a->radius );
         angle += inc;
     }
-    
+
     a->pos[0] = (Flt)( rand() % xres );
     a->pos[1] = (Flt)( rand() % yres );
     a->pos[2] = 0.0f;
@@ -141,27 +141,27 @@ void addAsteroid ()
 
     Flt tmpx = xres >> 2;
     Flt tmpy = yres >> 2;
-    
+
     if ( a->pos[0] < ( 3.0 * tmpx ) &&
-	    a->pos[0] > tmpx &&
-	    a->pos[1] < ( 3.0 * tmpy ) &&
-	    a->pos[1] > tmpy
+            a->pos[0] > tmpx &&
+            a->pos[1] < ( 3.0 * tmpy ) &&
+            a->pos[1] > tmpy
        ) //protect area near center from bigger asteroids
     {
 
-	if ( a->pos[0] < ( 2.0 * tmpx ) &&
-		a->pos[0] > tmpx
-	   ) //left half
-	{
-	    a->pos[0] -= tmpx;
-	    a->pos[2] = 0.0f;
-	}
+        if ( a->pos[0] < ( 2.0 * tmpx ) &&
+                a->pos[0] > tmpx
+           ) //left half
+        {
+            a->pos[0] -= tmpx;
+            a->pos[2] = 0.0f;
+        }
 
-	else //right half
-	{
-	    a->pos[0] += tmpx;
-	    a->pos[2] = 0.0f;
-	}
+        else //right half
+        {
+            a->pos[0] += tmpx;
+            a->pos[2] = 0.0f;
+        }
 
     }
 
@@ -174,7 +174,7 @@ void addAsteroid ()
 
     else
     {
-	a->color[0] = 0.3;
+        a->color[0] = 0.3;
         a->color[1] = 0.4;
         a->color[2] = 0.5;
     }
@@ -190,7 +190,7 @@ void addAsteroid ()
 
     if (g.ahead != NULL)
     {
-    	g.ahead->prev = a;
+        g.ahead->prev = a;
     }
 
     g.ahead = a;
@@ -208,45 +208,45 @@ void deleteAsteroid( Asteroid *node )
     if ( !g.done )
     {
 
-	if ( node )
-	{
+        if ( node )
+        {
 
-	    if ( node->prev == NULL )
-	    {
+            if ( node->prev == NULL )
+            {
 
-		if ( node->next == NULL )
-		{
-		    g.ahead = NULL;
-		}
+                if ( node->next == NULL )
+                {
+                    g.ahead = NULL;
+                }
 
-		else
-		{
-		    node->next->prev = NULL;
-		    g.ahead = node->next;
-		}
+                else
+                {
+                    node->next->prev = NULL;
+                    g.ahead = node->next;
+                }
 
-	    }
+            }
 
-	    else
-	    {
+            else
+            {
 
-		if ( node->next == NULL )
-		{
-		    node->prev->next = NULL;
-		}
+                if ( node->next == NULL )
+                {
+                    node->prev->next = NULL;
+                }
 
-		else
-		{
-		    node->prev->next = node->next;
-		    node->next->prev = node->prev;
-		}
+                else
+                {
+                    node->prev->next = node->next;
+                    node->next->prev = node->prev;
+                }
 
-	    }
+            }
 
-	    delete node;
-	    node = NULL;
+            delete node;
+            node = NULL;
 
-	}
+        }
     }
 
 }
@@ -260,13 +260,13 @@ void asteroidRadiusSpeed( Asteroid *a )
     cout << "ayVel: " << a->vel[1] << endl;
 
     cout << a->radius << ": asteroid radius\n";
-    
+
 
     Flt radiusVel = ( ( 500.0 / ( a->radius + 100.0 ) ) - 2.0 );
 
     if ( radiusVel < 0 )
     {
-	radiusVel = 0.05;
+        radiusVel = 0.05;
     }
 
     Flt xVel = a->vel[0];
@@ -285,8 +285,8 @@ void asteroidRadiusSpeed( Asteroid *a )
     cout << "axVel: " << a->vel[0] << endl;
     cout << "ayVel: " << a->vel[1] << endl;
     cout << "adjusted speed: " 
-	<< sqrt( ( a->vel[0] * a->vel[0] ) + ( a->vel[1] * a->vel[1] ) ) 
-	<< endl;
+        << sqrt( ( a->vel[0] * a->vel[0] ) + ( a->vel[1] * a->vel[1] ) ) 
+        << endl;
 
 }
 
@@ -299,13 +299,13 @@ void shipRadiusSpeed()
     cout << g.ship.vel[1] << " ShipYVel\n";
 
     cout << g.ship.radius << ": ship radius\n";
-    
+
 
     Flt radiusVel = ( ( 500.0 / ( g.ship.radius + 100.0 ) ) - 2.0 );
 
     if ( radiusVel < 0 )
     {
-	radiusVel = 0.01;
+        radiusVel = 0.01;
     }
 
     Flt xVel = g.ship.vel[0];
@@ -324,8 +324,8 @@ void shipRadiusSpeed()
     cout << g.ship.vel[0] << " ShipXVel\n";
     cout << g.ship.vel[1] << " ShipYVel\n";
     cout << "adjusted speed: " 
-	<< sqrt( ( g.ship.vel[0] * g.ship.vel[0] ) + ( g.ship.vel[1] * g.ship.vel[1] ) ) 
-	<< endl;
+        << sqrt( ( g.ship.vel[0] * g.ship.vel[0] ) + ( g.ship.vel[1] * g.ship.vel[1] ) ) 
+        << endl;
 
 }
 
@@ -334,37 +334,37 @@ int smithCheckKeys( XEvent *e )
 {
     static int shift=0;
     int key = XLookupKeysym(&e->xkey, 0);
-    
+
     if (e->type == KeyRelease)
     {
 
-	keys[key]=0;
-	if (key == XK_Shift_L || 
-		key == XK_Shift_R)
-	{
-	    shift=0;
-	}
+        keys[key]=0;
+        if (key == XK_Shift_L || 
+                key == XK_Shift_R)
+        {
+            shift=0;
+        }
 
-	return 0;
+        return 0;
 
     }
-    
+
     if (e->type == KeyPress)
     {
-	keys[key]=1;
+        keys[key]=1;
 
-	if (key == XK_Shift_L || 
-		key == XK_Shift_R) 
-	{
-	    shift=1;
-	    return 0;
-	}
+        if (key == XK_Shift_L || 
+                key == XK_Shift_R) 
+        {
+            shift=1;
+            return 0;
+        }
 
     }
-    
+
     else
     {
-	return 0;
+        return 0;
     }
 
     if (shift)
@@ -374,16 +374,16 @@ int smithCheckKeys( XEvent *e )
     switch(key)
     {
 
-	case XK_r:
-	    if ( g.gameOver )
-	    {
-		remakeGame();	
-	    }
+        case XK_r:
+            if ( g.gameOver )
+            {
+                remakeGame();	
+            }
 
-	    break;
-	    
+            break;
+
     }
-    
+
     return 0;
 
 }
@@ -392,24 +392,24 @@ int smithCheckKeys( XEvent *e )
 void deleteAllAsteroid()
 {
 
-	while ( g.ahead != NULL )
-	{
-	    Asteroid *temp = g.ahead->next;
-	    delete g.ahead;
-	    g.ahead = temp;
-	    g.nasteroids--;
-	    
-	    if (g.ahead == NULL)
-	    {
-		break;
-	    }
+    while ( g.ahead != NULL )
+    {
+        Asteroid *temp = g.ahead->next;
+        delete g.ahead;
+        g.ahead = temp;
+        g.nasteroids--;
 
-	}
+        if (g.ahead == NULL)
+        {
+            break;
+        }
 
-	if ( g.nasteroids == 0 )
-	{
-	    cout << "all asteroids have been properly deleted\n";
-	}
+    }
+
+    if ( g.nasteroids == 0 )
+    {
+        cout << "all asteroids have been properly deleted\n";
+    }
 
 }
 
